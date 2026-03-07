f99b74896ac30e4ae6a838b5c3730fa28bb24c58
Noddy <nishant.verma04@yahoo.com>
2026-03-07 15:55:03 +0530

    Fix: Remove leftover gallery module imports in main.js

--- src/main.js
+++ src/main.js
@@ -20,7 +20,6 @@ import { galleryItems } from './data/gallery.js';
 
 // Global API for inline HTML handlers (onClick)
 window.appAPI = {
-  openLightbox: (index) => openLightbox(galleryItems, index),
   openEventSpecificLightbox: (images, index) => openLightbox(images, index),
   openVideo: (youtubeId) => openVideoModal(youtubeId)
 };
